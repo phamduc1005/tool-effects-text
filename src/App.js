c1f0742a383c53b5aa3354525eea5b48ce601366
phamduc1005 <duchungpham10052001@gmail.com>
2022-09-06 13:44:04 +0700

    hollow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import "../src/components/hollow/style.scss"
+import TextField from '@mui/material/TextField';
 
 function App() {
+  const [currentEffect, setCurrentEffect] = React.useState("")
+
+  const [thickness, setThickness] = React.useState(50)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 100}}>
+      <Stack sx={{ marginBottom: 5 }} direction="row" spacing={5}>
+        <Button variant="contained" onClick={() => setCurrentEffect("hollow")}>Hollow</Button>
+        <Button variant="contained" onClick={() => setCurrentEffect("splice")}>Splice</Button>
+      </Stack>
+
+      {currentEffect === "hollow" && 
+      <TextField 
+      id="thickness" 
+      label="Thickness" 
+      variant="standard" 
+      value={thickness} 
+      onChange={(e) => setThickness(e.target.value)}
+      />
+      }
+
+      <div style={{ margin: '120px 0px 0px 550px' }}>
+        <div className={currentEffect}>
+          <div 
+          style={ currentEffect ? 
+          currentEffect === "hollow" ? { WebkitTextStroke: thickness/18.661242466 } : {} 
+          : {} 
+          }
+          >
+            <h1>Thank kiu nhe !!</h1>      
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }

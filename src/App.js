514b05664a0c214a9de97c5b7f0d63a07707e3f1
phamduc1005 <duchungpham10052001@gmail.com>
2022-09-07 15:40:43 +0700

    echo

--- src/App.js
+++ src/App.js
@@ -2,116 +2,165 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import "../src/components/hollow/style.scss"
-import "../src/components/splice/style.scss"
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import "../src/components/hollow/style.scss"
+import "../src/components/splice/style.scss"
+import "../src/components/echo/style.scss"
 
-function App() {
-  const [currentEffect, setCurrentEffect] = React.useState("")
-  const [hollow, setHollow] = React.useState({
-    thickness: 50
-  });
-  const [splice, setSplice] = React.useState({
-    thickness: 50,
-    offset: 50,
-    direction: -45,
-    color: "rgb(128, 128, 128)"
-  })
+const App = () => {
+	const [currentEffect, setCurrentEffect] = React.useState("");
+	const [hollow, setHollow] = React.useState({
+		thickness: 50
+	});
+	const [splice, setSplice] = React.useState({
+		thickness: 50,
+		offset: 50,
+		direction: -45,
+		color: "rgb(128, 128, 128)"
+	});
 
-  const toRadians = (angleDegrees) => {
-    return angleDegrees * (Math.PI / 180);
-  };
+    const [echo, setEcho] = React.useState({
+        offset: 50,
+        direction: -45,
+        color: "rgb(128, 128, 128"
+    });
+
+	const toRadians = (angleDegrees) => {
+		return angleDegrees * (Math.PI / 180);
+	};
 
 
-  return (
-    <div style={{ margin: 100}}>
-      <Stack sx={{ marginBottom: 5 }} direction="row" spacing={5}>
-        <Button variant="contained" color='error' onClick={() => setCurrentEffect("")}>Start Over</Button>
-        <Button variant="contained" onClick={() => setCurrentEffect("hollow")}>Hollow</Button>
-        <Button variant="contained" onClick={() => setCurrentEffect("splice")}>Splice</Button>
-      </Stack>
+	return (
+		<div style={{ margin: 100}}>
+			<Stack sx={{ marginBottom: 5 }} direction="row" spacing={5}>
+				<Button variant="contained" color='error' onClick={() => setCurrentEffect("")}>Start Over</Button>
+				<Button variant="contained" onClick={() => setCurrentEffect("hollow")}>Hollow</Button>
+				<Button variant="contained" onClick={() => setCurrentEffect("splice")}>Splice</Button>
+                <Button variant="contained" onClick={() => setCurrentEffect("echo")}>Echo</Button>
+			</Stack>
 
-      {currentEffect === "hollow" && 
-      <TextField 
-      id="thickness" 
-      label="Thickness" 
-      variant="standard" 
-      value={hollow.thickness} 
-      onChange={(e) => setHollow({...hollow, thickness: e.target.value})}
-      />
-      }
+			{currentEffect === "hollow" && 
+			<TextField 
+			id="thickness" 
+			label="Thickness" 
+			variant="standard" 
+			value={hollow.thickness} 
+			onChange={(e) => setHollow({...hollow, thickness: e.target.value})}
+			/>
+			}
 
-      {currentEffect === "splice" && 
-      <Box>
-        <TextField 
-        id="thickness" 
-        label="Thickness" 
-        variant="standard" 
-        value={splice.thickness} 
-        onChange={(e) => setSplice({...splice, thickness: e.target.value})}
-        />
-        <TextField 
-        sx={{ marginLeft: 5 }}
-        id="offset" 
-        label="Offset" 
-        variant="standard" 
-        value={splice.offset} 
-        onChange={(e) => setSplice({...splice, offset: e.target.value})}
-        />
-        <TextField 
-        sx={{ marginLeft: 5 }}
-        id="direction" 
-        label="Direction" 
-        variant="standard" 
-        value={splice.direction} 
-        onChange={(e) => setSplice({...splice, direction: e.target.value})}
-        />
-        <FormControl variant="standard" sx={{ marginLeft: 5 }}>
-          <InputLabel id="label-splice-color">Color</InputLabel>
-          <Select
-            labelId="label-splice-color"
-            id="select-splice-color"
-            value={splice.color}
-            label="Color"
-            onChange={(e) => setSplice({...splice, color: e.target.value})}
-          >
-            <MenuItem value={"rgb(128, 128, 128)"}>Gray</MenuItem>
-            <MenuItem value={"rgb(255, 0, 0)"}>Red</MenuItem>
-            <MenuItem value={"rgb(0, 255, 0)"}>Green</MenuItem>
-            <MenuItem value={"rgb(0, 0, 255)"}>Blue</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      }
+			{currentEffect === "splice" && 
+			<Box>
+				<TextField 
+				id="thickness" 
+				label="Thickness" 
+				variant="standard" 
+				value={splice.thickness} 
+				onChange={(e) => setSplice({...splice, thickness: e.target.value})}
+				/>
+				<TextField 
+				sx={{ marginLeft: 5 }}
+				id="offset" 
+				label="Offset" 
+				variant="standard" 
+				value={splice.offset} 
+				onChange={(e) => setSplice({...splice, offset: e.target.value})}
+				/>
+				<TextField 
+				sx={{ marginLeft: 5 }}
+				id="direction" 
+				label="Direction" 
+				variant="standard" 
+				value={splice.direction} 
+				onChange={(e) => setSplice({...splice, direction: e.target.value})}
+				/>
+				<FormControl variant="standard" sx={{ marginLeft: 5 }}>
+                    <InputLabel id="label-splice-color">Color</InputLabel>
+                    <Select
+                        labelId="label-splice-color"
+                        id="select-splice-color"
+                        value={splice.color}
+                        label="Color"
+                        onChange={(e) => setSplice({...splice, color: e.target.value})}
+                    >
+                        <MenuItem value={"rgb(128, 128, 128)"}>Gray</MenuItem>
+                        <MenuItem value={"rgb(255, 0, 0)"}>Red</MenuItem>
+                        <MenuItem value={"rgb(0, 255, 0)"}>Green</MenuItem>
+                        <MenuItem value={"rgb(0, 0, 255)"}>Blue</MenuItem>
+                    </Select>
+				</FormControl>
+			</Box>
+			}
 
-      <div style={{ margin: '120px 0px 0px 550px' }}>
-        <div className={currentEffect}>
-          <div 
-          style={ 
-          currentEffect ? 
-            currentEffect === "hollow" ? 
-            { WebkitTextStroke: ((hollow.thickness - 1) * 0.075 + 0.825).toFixed(3) } 
-            : 
-            currentEffect === "splice" ? 
-            { 
-              WebkitTextStroke: ((splice.thickness - 1) * 0.075 + 0.825).toFixed(3), 
-              textShadow: `${((splice.offset * 0.15) * -(Math.sin(toRadians(splice.direction)))).toFixed(7)}px ${((splice.offset * 0.15) * Math.cos(toRadians(splice.direction))).toFixed(7)}px 0px ${splice.color}`
-            } 
-            : {} 
-          : {} 
-          }
-          >
-            <h1>Thank kiu nhe !!</h1>      
-          </div>
-        </div>
-      </div>
+            {currentEffect === "echo" &&
+            <Box>
+                <TextField 
+				id="offset" 
+				label="Offset" 
+				variant="standard" 
+				value={echo.offset} 
+				onChange={(e) => setEcho({...echo, offset: e.target.value})}
+				/>
+                <TextField 
+				sx={{ marginLeft: 5 }}
+				id="direction" 
+				label="Direction" 
+				variant="standard" 
+				value={echo.direction} 
+				onChange={(e) => setEcho({...echo, direction: e.target.value})}
+				/>
+                <FormControl variant="standard" sx={{ marginLeft: 5 }}>
+                    <InputLabel id="label-echo-color">Color</InputLabel>
+                    <Select
+                        labelId="label-echo-color"
+                        id="select-echo-color"
+                        value={echo.color}
+                        label="Color"
+                        onChange={(e) => setEcho({...echo, color: e.target.value})}
+                    >
+                        <MenuItem value={"rgb(128, 128, 128"}>Gray</MenuItem>
+                        <MenuItem value={"rgb(255, 0, 0"}>Red</MenuItem>
+                        <MenuItem value={"rgb(0, 255, 0"}>Green</MenuItem>
+                        <MenuItem value={"rgb(0, 0, 255"}>Blue</MenuItem>
+                    </Select>
+				</FormControl>
+            </Box>
+            }
 
-    </div>
-  )
+			<div style={{ margin: '120px 0px 0px 550px' }}>
+				<div className={currentEffect}>
+					<div 
+					style={ 
+					currentEffect ? 
+						currentEffect === "hollow" ? 
+						{ WebkitTextStroke: ((hollow.thickness - 1) * 0.075 + 0.825).toFixed(3) } 
+						: 
+						currentEffect === "splice" ? 
+						{ 
+						    WebkitTextStroke: ((splice.thickness - 1) * 0.075 + 0.825).toFixed(3), 
+						    textShadow: `${((splice.offset * 0.15) * -(Math.sin(toRadians(splice.direction)))).toFixed(7)}px ${((splice.offset * 0.15) * Math.cos(toRadians(splice.direction))).toFixed(7)}px 0px ${splice.color}`
+						} 
+                        : 
+                        currentEffect === "echo" ?
+                        { 
+                            textShadow: `${((echo.offset * 0.15) * -(Math.sin(toRadians(echo.direction)))).toFixed(7)}px ${((echo.offset * 0.15) * Math.cos(toRadians(echo.direction))).toFixed(7)}px 0px ${echo.color}, 0.5), 
+                                        ${(((echo.offset * 0.15) * -(Math.sin(toRadians(echo.direction)))).toFixed(7))* 2}px ${(((echo.offset * 0.15) * Math.cos(toRadians(echo.direction))).toFixed(7))* 2}px 0px ${echo.color}, 0.3) ` 
+                        }
+						: {} 
+					: {} 
+					}
+					>
+						<h1 style={{ fontSize: 50 }}>Thank kiu nhe !!</h1>      
+					</div>
+				</div>
+			</div>
+
+		</div>
+	)
 };
 
 export default App;
